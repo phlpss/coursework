7ac15f3ecb151e26649773ab183028850fd2eb65
phlpss <kateryna.filip.pz.2022@lpnu.ua>
2023-11-18 23:44:31 +0200

    add handle to coursename_comboBox

--- src/main/java/com/example/coursework/controller/GroupsInCourseController.java
+++ src/main/java/com/example/coursework/controller/GroupsInCourseController.java
@@ -4,27 +4,33 @@ import com.example.coursework.ObjectFactory;
 import com.example.coursework.repository.GroupRepository;
 import com.example.coursework.other.Group;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.input.DragEvent;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import static com.example.coursework.controller.util.SceneSwitcher.switchScene;
 
 public class GroupsInCourseController {
 
     GroupRepository groupRepository = ObjectFactory.groupRepository();
+
+    @FXML
+    public void initialize() {
+        List<String> courses = groupRepository.getCourses();
+        coursename_comboBox.setItems(FXCollections.observableArrayList(courses));
+
+        List<Integer> years = Arrays.asList(1, 2, 3, 4);
+        ObservableList<Integer> observableYears = FXCollections.observableArrayList(years);
+        courseYear_comboBox.setItems(observableYears);
+    }
 
     @FXML
     public void switchToMainPage(ActionEvent event) throws IOException {
@@ -37,13 +43,10 @@ public class GroupsInCourseController {
     }
 
     @FXML
-    private ComboBox<?> courseYear_comboBox;
+    private ComboBox<Integer> courseYear_comboBox;
 
     @FXML
-    private ComboBox<?> coursename_comboBox;
-
-    @FXML
-    private Button manageGroup_Button;
+    private ComboBox<String> coursename_comboBox;
 
     @FXML
     private ListView<Group> groupsByCourseName_ListView;
@@ -51,22 +54,10 @@ public class GroupsInCourseController {
     @FXML
     private ListView<?> groupsByCourseYear_ListView;
 
-    private void loadCourseNames() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        File file = new File("C:\\Users\\KATERYNKA\\Desktop\\LPNU\\term 3\\CouseWork\\coursework\\src\\main\\resources\\json\\groups.json");
-
-        List<Group> groups = groupRepository.getGroups();
-
-        Set<String> courseNames = new HashSet<>();
-        for (Group group : groups) {
-            courseNames.addAll(group.getCourses());
-        }
-        coursename_comboBox.setItems(FXCollections.observableArrayList());
-    }
 
     @FXML
-    void getGroupsByCourseName_EnterKey(ActionEvent event) {
-        String selectedCourse = (String) coursename_comboBox.getSelectionModel().getSelectedItem();
+    void getGroupsByCourseName_EnterKey() {
+        String selectedCourse = coursename_comboBox.getSelectionModel().getSelectedItem();
         if (selectedCourse != null) {
             List<Group> groupsByCourseName = groupRepository.getGroupsByCourseName(selectedCourse);
             groupsByCourseName_ListView.getItems().clear();
@@ -76,7 +67,7 @@ public class GroupsInCourseController {
 
 
     @FXML
-    void getGroupsByCourseYear_EnterKey(DragEvent event) {
+    void getGroupsByCourseYear_EnterKey() {
 //        groupRepository.getGroupsByCourseYear(/* course year user chose */);
     }
 }
